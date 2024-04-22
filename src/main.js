278774277a675f6be8b91e09d589bcca477208b4
Raphael Porto <raphael_port@outlook.com>
2024-04-22 14:30:09 -0300

    feat: adicionado os schedules e o enfileiramento das candles criadas a partir dos requests

--- src/main.js
+++ src/main.js
@@ -1,22 +1,44 @@
 import "dotenv/config.js"
-import Candle from "./models/candle.js"
-import { CURRENCY } from "./enums.js"
+import schedule from "node-schedule"
+import { CRON_TIME, CURRENCY } from "./enums.js"
+import { producer_candles } from "./amqp/producer-candles.js"
+import RabbitMQ from "./amqp/rabbitmq.js"
 
-const getValueBTC = async (region) => {
+const get_values_btc = async () => {
   const response = await fetch(process.env.BLOCKCHAIN_VALUES)
-  if (response.ok) {
-    if (region) {
-      const json = await response.json()
-      return json[region]
-    }
+  if (response.ok)
     return response.json()
-  }
   throw new Error("Erro ao recolher valores bitcoin.")
 }
 
+let valuesBTC = null;
 
-const generateCandle = async () => {
+const jobRefreshValuesBTC = schedule.scheduleJob(CRON_TIME.SEGUNDO_TRINTA, async () => {
+  console.log("-------------------------")
+  console.log("Atualizando os valores do Bitcoin.")
+  valuesBTC = await get_values_btc();
+  console.log("Valores atulizados com sucesso.")
+  console.log("-------------------------")
+  console.log("\n")
+})
 
-}
-
-
+let lastValues = new Array(Object.keys(CURRENCY).length).fill(0);
+const jobRowUpCandles = schedule.scheduleJob(CRON_TIME.SEGUNDO_CINQUENTA, async () => {
+  for (const [i, v] of Object.values(CURRENCY).entries()) {
+    console.log(`----------/${v}/----------`)
+    console.log(`Enfileirando BTC/${v}.`)
+    const rbtmq = await RabbitMQ.init();
+    const channel = await rbtmq.create_channel();
+    const newValue = await producer_candles(
+      v,
+      valuesBTC,
+      lastValues[i],
+      channel
+    )
+    rbtmq.close_connection();
+    lastValues[i] = newValue || 0;
+    console.log("Enfileirado com sucesso.")
+    console.log("-------------------------")
+    console.log("\n")
+  }
+})
